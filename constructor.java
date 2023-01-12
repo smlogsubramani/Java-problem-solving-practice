a91c43a482427cbe6c2572659d119fa89aef290c
Logasubramani sm <smlogasubramani@gmail.com>
2023-01-12 19:19:40 +0530

    constructor updated

---
+++ constructor.java
@@ -0,0 +1,50 @@
+// public class constructor {
+
+//     int modelyear;
+//     String modelname;
+
+//     public constructor(int year, String name){
+//         modelyear = year;
+//         modelname = name;
+//     }
+//     public static void main(String args[]){
+//         constructor loga = new constructor(1898, "mustang");
+//         System.out.println(loga.modelyear+" "+loga.modelname);
+//     }
+// }
+
+
+
+// public class constructor{
+//     int x;
+//     public constructor(){
+//         x = 5;
+
+//     }
+
+//     public static void main(String args[]){
+//         constructor myobj = new constructor();
+//         System.out.println(myobj.x);
+//     }
+// }
+
+
+
+
+//methods using public not static
+//public needs an object 
+//static doesnt need an object
+
+
+
+// public class constructor{
+//     public int loga(int x){
+//         return x;
+//     }
+
+//     public static void main(String args[]){
+//         constructor myobj = new constructor();
+//         System.out.println(myobj.loga(5));
+//     }
+
+// }
