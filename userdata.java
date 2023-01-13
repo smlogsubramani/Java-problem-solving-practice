98bb90836db5a05609ed81ab2bc1d80285f1168f
Logasubramani sm <smlogasubramani@gmail.com>
2023-01-13 09:38:59 +0530

    user data

---
+++ userdata.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;   
+public class userdata {
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the name");
+    String Name = sc.nextLine();
+    System.out.println("username = "+Name);    
+    }
+}
