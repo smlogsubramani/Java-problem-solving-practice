e4ab0676f9a99fd00b2ef92c23b3e72f26af3b3b
logasubramanism <smlogasubramani@gmail.com>
2023-01-15 09:30:16 +0530

    updated model

--- profile1.java
+++ profile1.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
+//datamodel
 
 public class profile1{
     public static ArrayList<person> arr = new ArrayList<person>();
@@ -9,23 +9,53 @@ public class profile1{
         person myobj = new person();
         {
           Scanner sc = new Scanner(System.in);
+          System.out.println("Enter the name");
           myobj.setname(sc.nextLine());
+          System.out.println("Enter the Address");
           myobj.setAddress(sc.nextLine());
+          System.out.println("Enter the Phonenumber");
+          myobj.setphonenumber(sc.nextLine());
           arr.add(myobj);
 
         }
     }
     public static void main(String args[]){
         
-    for(int i=0;i<=4;i++)
+        
+    for(int i=0;i<2;i++)
     {
         GETINPUT();
     }
-       for (person i : arr ) {
+    //    for (person i : arr ) {
         
-            System.out.println(i.getname() +" "+ i.getAddress());
+    //         System.out.println(i.getname() +" "+ i.getAddress()+" "+ i.getphonenumber());
+    //     }
+
+        try (Scanner findscanner = new Scanner(System.in)) {
+            System.out.println("Enter the mobile number");
+            String mobnumber = findscanner.nextLine();
+            person matchperson = arr.stream().filter(person->person.getphonenumber().equals(mobnumber)).findFirst().orElse(null);
+            if(matchperson==null){
+                System.out.println("There is no person with this mobilenumber");
+
+            }
+            else
+            {
+                System.out.println("The details of the entered phone number is");
+                System.out.println(matchperson.getname());
+                System.out.println(matchperson.getAddress());
+                System.out.println(matchperson.getphonenumber());
+            }
+       
+       
+       
         }
-         
+    
+           
+        
+
+
+
         } 
 
     }
